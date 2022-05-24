039bace1a12e6619f15a7b5eb7d90cc009782d66
mouhcine <mouhcine15.00@gmail.com>
2022-05-24 16:44:26 +0200

    es completo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const root = new Vue({
     el: "#container",
     data: {
         text: "un messaggio all'interno di un h1",
-        immagine: "https://www.lenny76.com/wp-content/uploads/2018/03/hello-world.jpg"
+        immagine: "https://www.lenny76.com/wp-content/uploads/2018/03/hello-world.jpg",
+        size: "resize"
     },
 });
